30a65344a7508836a7224bbeedca9da2f224c0fd
rickard cruz <rickard cruz>
2016-05-11 08:57:47 +0200

    Interrupt shit

--- alarm.ino
+++ alarm.ino
@@ -1,10 +1,10 @@
 
 static int wasTriggered = 0;
-static int alarmPin;
-
 
 void alarm_start(const int pin){
   // set interrupt och alarmPin till pin
+    attachInterrupt(digitalPinToInterrupt(pin), alarm_interrupt, FALLING);
+  
 }
 
 int alarm_triggered()
@@ -12,8 +12,9 @@ int alarm_triggered()
   return wasTriggered;
 }
 
-
 void alarm_reset(){
   wasTriggered = 0;
 }
-
+void alarm_interrupt(){
+  wasTriggered = 1;
+  }
