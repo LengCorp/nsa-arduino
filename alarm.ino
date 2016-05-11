2e3e22bec4f9975a8da5d9f8c2ef7210430b0211
Emil Lengman <emillen@kth.se>
2016-05-11 09:28:04 +0200

    formatting, removing comment, and adding comments

--- alarm.ino
+++ alarm.ino
@@ -1,20 +1,31 @@
 
 static int wasTriggered = 0;
 
-void alarm_start(const int pin){
-  // set interrupt och alarmPin till pin
+/**
+ * Initializes the alarm to use a specific pin 
+ */
+void alarm_start(const int pin)
+{
     attachInterrupt(digitalPinToInterrupt(pin), alarm_interrupt, RISING);
-  
 }
 
+/**
+ * Checks if the alarm has been triggered since last check 
+ */
 int alarm_triggered()
 {
   return wasTriggered;
 }
 
-void alarm_reset(){
+/**
+ * Resets the alarm to untriggered state
+ */
+void alarm_reset()
+{
   wasTriggered = 0;
 }
-void alarm_interrupt(){
+
+void alarm_interrupt()
+{
   wasTriggered = 1;
-  }
+}
